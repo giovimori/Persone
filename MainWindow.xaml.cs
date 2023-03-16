bd97131c5dbcfa9c9383bcabc2c5058766fddf9d
Giovanni Morelli <116790906+giovimori@users.noreply.github.com>
2023-03-16 10:23:43 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using morelli.giovanni._4h.persone.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace morelli.giovanni._4h.persone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow ()
+        {
+            InitializeComponent ();
+            ATA a = new ATA ();
+            Console.WriteLine(a);
+        }
+
+        private void dgDati_LoadingRow (object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnApriFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ Persone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace morelli.giovanni._4h.persone.Models
+{
+    public enum TipoPersona { NonDefinito,ATA,Alunno,Docente};
+    public class Persone
+    {
+        public TipoPersona Tipo { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ATA : Persone
+    {
+        public ATA() 
+        {
+            Tipo = TipoPersona.ATA;        
+        }
+
+        public ATA( string nomeFile ) { }
+    }
+}
+
